3ad68700b5072c2c7e82a4dcac886e95fbced403
Wolfram Fischer <Wolfram.Fischer1979@gmail.com>
2018-06-27 23:04:24 +0200

    Initial import of spring initializer for 01 saying hello

---
+++ 01-saying-hello/src/main/java/com/copyonwrite/sayinghello/SayinghelloApplication.java
@@ -0,0 +1,12 @@
+package com.copyonwrite.sayinghello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayinghelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayinghelloApplication.class, args);
+	}
+}
